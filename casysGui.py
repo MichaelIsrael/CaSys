2924580f643a009fff6043f4e42a5ef416f69536
Michael M. Behman <michael.behman@gmail.com>
2013-11-25 11:34:26 +0100

    Add initial GUI file.

---
+++ casysGui.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+#
+# Copyright 2013-2014 Michael Israel
+#
+# This file is part of Casys.
+#
+# Casys is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# Casys is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Casys.  If not, see <http://www.gnu.org/licenses/>.Copyright.
+"""
+This is the gui of Casys.
+"""
+
+
